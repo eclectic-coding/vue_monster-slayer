2033aea4cb4f75b9d004917c5e7fefef070ab360
eclecticcoding <chuck@eclecticsaddlebag.com>
2021-07-02 06:05:26 -0400

    Set up attacks methods

---
+++ app.js
@@ -0,0 +1,23 @@
+function getRandomValue(min, max) {
+  return Math.floor(Math.random() * (max - min)) + min;
+}
+
+const app = Vue.createApp({
+  data() {
+    return {
+      monsterHealth: 100,
+      playerHealth: 100
+    };
+  },
+  attackMonster() {
+    const attackValue = getRandomValue(5, 12);
+    this.monsterHealth -= attackValue;
+    this.attackPlayer();
+  },
+  attackPlayer() {
+    const attackValue = getRandomValue(8, 15);
+    this.playerHealth -= attackValue;
+  }
+});
+
+app.mount('#game');
